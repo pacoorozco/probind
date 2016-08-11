245c7ed7ce8541262226b3a2e9964f6689a1c2f5
Paco Orozco <paco@pacoorozco.info>
2016-08-11 11:13:19 +0200

    feat: Add Registry dependency and module
    This module will be used to deal with app settings.

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
          */
         Collective\Html\HtmlServiceProvider::class,  // HTML helpers
         Yajra\Datatables\DatatablesServiceProvider::class, // DataTables helpers
+        Torann\Registry\RegistryServiceProvider::class, // Settings helpers
 
         /*
          * Application Service Providers...
@@ -188,43 +189,44 @@ return [
 
     'aliases' => [
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'App'        => Illuminate\Support\Facades\App::class,
+        'Artisan'    => Illuminate\Support\Facades\Artisan::class,
+        'Auth'       => Illuminate\Support\Facades\Auth::class,
+        'Blade'      => Illuminate\Support\Facades\Blade::class,
+        'Cache'      => Illuminate\Support\Facades\Cache::class,
+        'Config'     => Illuminate\Support\Facades\Config::class,
+        'Cookie'     => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'      => Illuminate\Support\Facades\Crypt::class,
+        'DB'         => Illuminate\Support\Facades\DB::class,
+        'Eloquent'   => Illuminate\Database\Eloquent\Model::class,
+        'Event'      => Illuminate\Support\Facades\Event::class,
+        'File'       => Illuminate\Support\Facades\File::class,
+        'Gate'       => Illuminate\Support\Facades\Gate::class,
+        'Hash'       => Illuminate\Support\Facades\Hash::class,
+        'Lang'       => Illuminate\Support\Facades\Lang::class,
+        'Log'        => Illuminate\Support\Facades\Log::class,
+        'Mail'       => Illuminate\Support\Facades\Mail::class,
+        'Password'   => Illuminate\Support\Facades\Password::class,
+        'Queue'      => Illuminate\Support\Facades\Queue::class,
+        'Redirect'   => Illuminate\Support\Facades\Redirect::class,
+        'Redis'      => Illuminate\Support\Facades\Redis::class,
+        'Request'    => Illuminate\Support\Facades\Request::class,
+        'Response'   => Illuminate\Support\Facades\Response::class,
+        'Route'      => Illuminate\Support\Facades\Route::class,
+        'Schema'     => Illuminate\Support\Facades\Schema::class,
+        'Session'    => Illuminate\Support\Facades\Session::class,
+        'Storage'    => Illuminate\Support\Facades\Storage::class,
+        'URL'        => Illuminate\Support\Facades\URL::class,
+        'Validator'  => Illuminate\Support\Facades\Validator::class,
+        'View'       => Illuminate\Support\Facades\View::class,
 
         /*
          * Third Party Alias
          */
-        'Form'      => Collective\Html\FormFacade::class,  // HTML Form helpers
-        'HTML'      => Collective\Html\HtmlFacade::class,  // HTML helpers
+        'Form'       => Collective\Html\FormFacade::class,  // HTML Form helpers
+        'HTML'       => Collective\Html\HtmlFacade::class,  // HTML helpers
         'Datatables' => Yajra\Datatables\Facades\Datatables::class, // DataTables helpers
+        'Registry'   => Torann\Registry\Facades\Registry::class, // Settings helpers
 
     ],
 
