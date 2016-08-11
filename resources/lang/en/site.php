5375f5d9179a577b4f9c7e85ad67c3c591d26413
Paco Orozco <paco@pacoorozco.info>
2016-08-11 08:13:09 +0200

    feat: Complete sidebar navigation options
    Signed-off-by: Paco Orozco <paco@pacoorozco.info>

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
-    'navigation' => 'ADMIN NAVIGATION',
-    'dashboard' => 'Dashboard',
-    'servers'   => 'Servers',
-    'zones'     => 'Zones',
-    'records'   => 'Records',
-
-
+    'navigation'   => 'ADMIN NAVIGATION',
+    'dashboard'    => 'Dashboard',
+    'servers'      => 'Servers',
+    'zones'        => 'Zones',
+    'records'      => 'Browse Records',
+    'push_updates' => 'Push Updates',
+    'tools'        => 'Tools',
+    'settings'     => 'Settings',
+    'configure'    => 'PROBIND CONFIGURATION',
+    'users'        => 'Users',
 ];
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -16,5 +16,33 @@
             <i class="fa fa-database"></i><span>{{ trans('site.zones') }}</span>
         </a>
     </li>
+    <li {!! (Request::is('records') ? ' class="active"' : '') !!}>
+        <a href="{{-- route('records.index') --}}">
+            <i class="fa fa-search"></i><span>{{ trans('site.records') }}</span>
+        </a>
+    </li>
+    <li {!! (Request::is('push') ? ' class="active"' : '') !!}>
+        <a href="{{-- route('push.index') --}}">
+            <i class="fa fa-download"></i><span>{{ trans('site.push_updates') }}</span>
+        </a>
+    </li>
+    <li {!! (Request::is('tools') ? ' class="active"' : '') !!}>
+        <a href="{{-- route('tools.index') --}}">
+            <i class="fa fa-wrench"></i><span>{{ trans('site.tools') }}</span>
+        </a>
+    </li>
+    
+    <li class="header">{{ trans('site.configure') }}</li>
+    <li {!! (Request::is('settings') ? ' class="active"' : '') !!}>
+        <a href="{{-- route('users.index') --}}">
+            <i class="fa fa-users"></i><span>{{ trans('site.users') }}</span>
+        </a>
+    </li>
+    <li {!! (Request::is('users') ? ' class="active"' : '') !!}>
+        <a href="{{-- route('settings.index') --}}">
+            <i class="fa fa-gears"></i><span>{{ trans('site.settings') }}</span>
+        </a>
+    </li>
+
 </ul>
 <!-- end: MAIN NAVIGATION MENU -->
