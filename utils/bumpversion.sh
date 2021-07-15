243b469747aae70976907848e2eec786baf35188
pacoorozco <paco@pacoorozco.info>
2021-07-15 13:10:15 +0200

    Update to latest version
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- utils/bumpversion.sh
+++ utils/bumpversion.sh
@@ -15,7 +15,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # ----------------------------------------------------------------------
-# This is a utility script for bumping the version on the ProBIND
+# This is a utility script for bumping the version on the Gamify
 # application. It uses `sed` to update the version variable in the
 # version file(s).
 #
@@ -28,17 +28,17 @@
 # ----------------------------------------------------------------------
 # Program name and version
 PN=$(basename "$0")
-VER='0.3'
+VER='0.4'
 # Root directory where files reside in
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # ----------------------------------------------------------------------
 # Functions
 # ----------------------------------------------------------------------
-usage()
-{
-	cat <<-EndUsage
-	Bump the version of ProBIND! (ver. ${VER})
+usage() {
+  cat <<-EndUsage
+	Bump the version of Gamify!
+	version ${VER}
 
 	usage: ${PN} [-h|--help] part
 
@@ -66,21 +66,20 @@ usage()
 	EndUsage
 }
 
-print_finish_release_note()
-{
-    #
-    # Get the current branch name
-    #
-    local _current_branch
-    _current_branch=$(git rev-parse --abbrev-ref HEAD)
+print_finish_release_note() {
+  #
+  # Get the current branch name
+  #
+  local _current_branch
+  _current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-    #
-    # Get last commit messages
-    #
-    local _last_commits
-    _last_commits=$(git log --pretty=short "v${current_version}".. | git shortlog | cat)
+  #
+  # Get last commit messages
+  #
+  local _last_commits
+  _last_commits=$(git log "v${current_version}..HEAD" --pretty=format:'* %B' --no-merges --reverse | cat)
 
-    cat <<-EndFinishReleaseNote
+  cat <<-EndFinishReleaseNote
     Remember to:
 
         1. Update CHANGELOG.md
@@ -91,59 +90,54 @@ print_finish_release_note()
 
         $ git commit -s -m "Bump version ${current_version} -> ${new_version}" config/app.php CHANGELOG.md
 
-        3. Merge to master branch:
-
-        $ git checkout master
-        $ git merge --no-ff ${_current_branch}
-
-        4. Tag the tree:
+        3. Tag the tree:
 
         $ git tag -s -m "Version ${new_version}" v${new_version}
+        $ git push origin ${_current_branch} --tags
 
-        5. Merge to develop branch:
+        4. Go to GitHub, create a PR and merge to the master branch:
 
-        $ git checkout develop
+        5. Update any development branch:
+
+        $ git checkout ${_current_branch}
         $ git merge --no-ff ${_current_branch}
 	EndFinishReleaseNote
 }
 
-get_new_version_number()
-{
-    #
-    # Set new version
-    #
-    case $1 in
-        major) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1 + 1, 0, 0); }') ;;
-        minor) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1, $2 + 1, 0); }') ;;
-        patch) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1, $2, $3 + 1); }') ;;
-    esac
+get_new_version_number() {
+  #
+  # Set new version
+  #
+  case $1 in
+  major) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1 + 1, 0, 0); }') ;;
+  minor) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1, $2 + 1, 0); }') ;;
+  patch) new_version=$(echo "${current_version}" | awk -F. '{ printf ("%d.%d.%d\n", $1, $2, $3 + 1); }') ;;
+  esac
 }
 
-get_current_version_number()
-{
-    #
-    # Get current version from config/app.php
-    #
-    current_version=$(grep "'version' => '\(.*\)'" "${ROOT_DIR}/config/app.php" |
-    	head -1 |
-    	awk '{ print $3 }' | sed "s/[',]//g")
+get_current_version_number() {
+  #
+  # Get current version from config/app.php
+  #
+  current_version=$(grep "'version' => '\(.*\)'" "${ROOT_DIR}/config/app.php" |
+    head -1 |
+    awk '{ print $3 }' | sed "s/[',]//g")
 }
 
-bump_version()
-{
-    #
-    # Format the current date for the README header
-    #
-    local _date
-    _date=$(date '+%Y-%m-%d')
+bump_version() {
+  #
+  # Format the current date for the README header
+  #
+  local _date
+  _date=$(date '+%Y-%m-%d')
 
-    echo -e "Bump version: ${current_version} -> ${new_version}\n"
+  echo -e "Bump version: ${current_version} -> ${new_version}\n"
 
-    # Update version number on config/app.php
-    sed -i "${ROOT_DIR}/config/app.php" -e "s/'version' => '\(.*\)'/'version' => '${new_version}'/g"
+  # Update version number on config/app.php
+  sed -i "${ROOT_DIR}/config/app.php" -e "s/'version' => '\(.*\)'/'version' => '${new_version}'/g"
 
-	# Create a new section on CHANGELOG
-	sed -i "${ROOT_DIR}/CHANGELOG.md" -e "s/^## Unreleased/## Unreleased\n\n## ${new_version} - ${_date}\n/g"
+  # Create a new section on CHANGELOG
+  sed -i "${ROOT_DIR}/CHANGELOG.md" -e "s/^## Unreleased/## Unreleased\n\n## ${new_version} - ${_date}\n/g"
 }
 
 # ----------------------------------------------------------------------
@@ -152,28 +146,28 @@ bump_version()
 
 get_current_version_number
 
-if [ "x${current_version}" == "x" ]; then
-    echo "ERROR: Can't determine the current version."
-    exit 1
+if [[ -z "${current_version}" ]]; then
+  echo "ERROR: Can't determine the current version."
+  exit 1
 fi
 
 case $1 in
-    -h|--help)
-        usage
-        ;;
-	major|minor|patch)
-    	if [ "x$(git status -s -uno)" != "x" ]; then
-        	echo 'ERROR: Uncommited changes in repository. Commit before bump version' 1>&2
-	        exit 1
-        fi
-	    get_new_version_number $1
-	    bump_version
-	    print_finish_release_note
-	    ;;
-	*)
-	    echo "ERROR: You have failed to specify what to do correctly. Use '${PN} --help' for more information."
-	    exit 1
-	    ;;
+-h | --help)
+  usage
+  ;;
+major | minor | patch)
+  if [[ -n "$(git status -s -uno)" ]]; then
+    echo 'ERROR: Uncommitted changes in repository. Commit before bump version' 1>&2
+    exit 1
+  fi
+  get_new_version_number "$1"
+  bump_version
+  print_finish_release_note
+  ;;
+*)
+  echo "ERROR: You have failed to specify what to do correctly. Use '${PN} --help' for more information."
+  exit 1
+  ;;
 esac
 
 exit 0
