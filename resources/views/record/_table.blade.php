c389f56928d64d24b65f0782720cf16a2fb66a5c
pacoorozco <paco@pacoorozco.info>
2021-07-19 15:45:02 +0200

    Make DataTables responsive
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- resources/views/record/_table.blade.php
+++ resources/views/record/_table.blade.php
@@ -3,26 +3,30 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/AdminLTE/datatables/dataTables.bootstrap.css') }}">
 @endpush
 
-<table id="records-table" class="table table-striped table-bordered">
-    <thead>
-    <tr>
-        <th class="col-md-4">{{ __('record/table.name') }}</th>
-        <th class="col-md-1">{{ __('record/table.ttl') }}</th>
-        <th class="col-md-1">{{ __('record/table.type') }}</th>
-        <th class="col-md-4">{{ __('record/table.data') }}</th>
-        <th class="col-md-2">{{ __('general.actions') }}</th>
-    </tr>
-    </thead>
-    <tfoot>
-    <tr>
-        <th class="col-md-4">{{ __('record/table.name') }}</th>
-        <th class="col-md-1">{{ __('record/table.ttl') }}</th>
-        <th class="col-md-1">{{ __('record/table.type') }}</th>
-        <th class="col-md-4">{{ __('record/table.data') }}</th>
-        <th class="col-md-2">{{ __('general.actions') }}</th>
-    </tr>
-    </tfoot>
-</table>
+<div class="container-fluid">
+    <div class="table-responsive">
+        <table id="records-table" class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>{{ __('record/table.name') }}</th>
+                <th>{{ __('record/table.ttl') }}</th>
+                <th>{{ __('record/table.type') }}</th>
+                <th>{{ __('record/table.data') }}</th>
+                <th>{{ __('general.actions') }}</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>{{ __('record/table.name') }}</th>
+                <th>{{ __('record/table.ttl') }}</th>
+                <th>{{ __('record/table.type') }}</th>
+                <th>{{ __('record/table.data') }}</th>
+                <th>{{ __('general.actions') }}</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
 
 {{-- Scripts --}}
 @push('scripts')
@@ -30,25 +34,25 @@
     <script type="text/javascript" src="{{ asset('vendor/AdminLTE/datatables/dataTables.bootstrap.min.js') }}"></script>
 
     <script>
-    $(function () {
-        $('#records-table').DataTable({
-            "processing": true,
-            "serverSide": true,
-            "ajax": "{{ route('zones.records.data', $zone->id) }}",
-            "columns": [
-                {data: "name"},
-                {data: "ttl", "orderable": false, "searchable": false},
-                {data: "type"},
-                {data: "data"},
-                {data: "actions", "orderable": false, "searchable": false}
-            ],
-            "aLengthMenu": [
-                [5, 10, 15, 20, -1],
-                [5, 10, 15, 20, "{{ __('general.all') }}"]
-            ],
-            // set the initial value
-            "iDisplayLength": 10
+        $(function () {
+            $('#records-table').DataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": "{{ route('zones.records.data', $zone->id) }}",
+                "columns": [
+                    {data: "name"},
+                    {data: "ttl", "orderable": false, "searchable": false},
+                    {data: "type"},
+                    {data: "data"},
+                    {data: "actions", "orderable": false, "searchable": false}
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "{{ __('general.all') }}"]
+                ],
+                // set the initial value
+                "iDisplayLength": 10
+            });
         });
-    });
-</script>
+    </script>
 @endpush
--- resources/views/user/_table.blade.php
+++ resources/views/user/_table.blade.php
@@ -3,46 +3,50 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/AdminLTE/datatables/dataTables.bootstrap.css') }}">
 @endpush
 
-<table id="users-table" class="table table-striped table-bordered">
-    <thead>
-    <tr>
-        <th class="col-md-4">{{ __('user/table.username') }}</th>
-        <th class="col-md-3">{{ __('user/table.name') }}</th>
-        <th class="col-md-3">{{ __('user/table.email') }}</th>
-        <th class="col-md-2">{{ __('general.actions') }}</th>
-    </tr>
-    </thead>
-    <tfoot>
-    <tr>
-        <th class="col-md-4">{{ __('user/table.username') }}</th>
-        <th class="col-md-3">{{ __('user/table.name') }}</th>
-        <th class="col-md-3">{{ __('user/table.email') }}</th>
-        <th class="col-md-2">{{ __('general.actions') }}</th>
-    </tr>
-    </tfoot>
-</table>
+<div class="container-fluid">
+    <div class="table-responsive">
+        <table id="users-table" class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>{{ __('user/table.username') }}</th>
+                <th>{{ __('user/table.name') }}</th>
+                <th>{{ __('user/table.email') }}</th>
+                <th>{{ __('general.actions') }}</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>{{ __('user/table.username') }}</th>
+                <th>{{ __('user/table.name') }}</th>
+                <th>{{ __('user/table.email') }}</th>
+                <th>{{ __('general.actions') }}</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
 
 {{-- Scripts --}}
 @push('scripts')
     <script type="text/javascript" src="{{ asset('vendor/AdminLTE/datatables/jquery.dataTables.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('vendor/AdminLTE/datatables/dataTables.bootstrap.min.js') }}"></script>
     <script>
-    $(function () {
-        $('#users-table').DataTable({
-            "ajax": "{{ route('users.data') }}",
-            "columns": [
-                {data: "username"},
-                {data: "name"},
-                {data: "email"},
-                {data: "actions", "orderable": false, "searchable": false}
-            ],
-            "aLengthMenu": [
-                [5, 10, 15, 20, -1],
-                [5, 10, 15, 20, "{{ __('general.all') }}"]
-            ],
-            // set the initial value
-            "iDisplayLength": 10
+        $(function () {
+            $('#users-table').DataTable({
+                "ajax": "{{ route('users.data') }}",
+                "columns": [
+                    {data: "username"},
+                    {data: "name"},
+                    {data: "email"},
+                    {data: "actions", "orderable": false, "searchable": false}
+                ],
+                "aLengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "{{ __('general.all') }}"]
+                ],
+                // set the initial value
+                "iDisplayLength": 10
+            });
         });
-    });
     </script>
 @endpush
