bac21e475963d3fb352b60cc51d4330307d246d1
pacoorozco <paco@pacoorozco.info>
2021-07-19 09:40:30 +0200

    Add Ajax get calls
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,8 +18,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Make ajax GET request.
+     *
+     * @param  string  $uri
+     *
+     * @return TestResponse
+     */
+    protected function ajaxGet(string $uri): TestResponse
+    {
+        return $this->withHeader('HTTP_X-Requested-With', 'XMLHttpRequest')
+            ->get($uri);
+    }
 }
