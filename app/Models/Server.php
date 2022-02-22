74d15f560ba4b4b79752aed56ec6e0c117d3f186
pacoorozco <paco@pacoorozco.info>
2022-02-22 12:51:06 +0100

    Fix missing $ in docblock
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -36,7 +36,7 @@ use Spatie\Activitylog\Traits\LogsActivity;
  * @property ServerType $type The type of this server.
  * @property bool $push_updates This flag determines if this server must be pushed with zone files.
  * @property bool $ns_record This flag determines if this server will be included as NS on zone files.
- * @property bool active         This flags determines if this server is active or inactive.
+ * @property bool $active This flags determines if this server is active or inactive.
  */
 class Server extends Model
 {
@@ -67,7 +67,7 @@ class Server extends Model
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
-            ->setDescriptionForEvent(fn (string $eventName) => trans('server/messages.activity.' . $eventName, [
+            ->setDescriptionForEvent(fn(string $eventName) => trans('server/messages.activity.' . $eventName, [
                 'hostname' => $this->hostname,
                 'type' => $this->type,
             ]));
