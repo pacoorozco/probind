5e0b578dbe641c744b96fd3afc92150c90ae4d84
pacoorozco <paco@pacoorozco.info>
2022-02-23 16:10:18 +0100

    Add return type to methods
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * Copyright (c) 2016-2022 Paco Orozco <paco@pacoorozco.info>
+ *
+ * This file is part of ProBIND v3.
+ *
+ * ProBIND v3 is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * ProBIND v3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ProBIND v3. If not,
+ * see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,7 +23,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -21,7 +38,7 @@ class CreateUsersTable extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
--- database/migrations/2016_08_31_090802_create_activity_log_table.php
+++ database/migrations/2016_08_31_090802_create_activity_log_table.php
@@ -1,33 +1,46 @@
 <?php
+/*
+ * Copyright (c) 2016-2022 Paco Orozco <paco@pacoorozco.info>
+ *
+ * This file is part of ProBIND v3.
+ *
+ * ProBIND v3 is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * ProBIND v3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ProBIND v3. If not,
+ * see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateActivityLogTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+    public function up(): void
     {
-        Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('log_name')->nullable();
-            $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
-            $table->nullableMorphs('causer', 'causer');
-            $table->json('properties')->nullable();
-            $table->uuid('batch_uuid')->nullable();
-            $table->string('event')->nullable();
-            $table->timestamps();
-            $table->index('log_name');
-        });
+        Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'),
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('log_name')->nullable();
+                $table->text('description');
+                $table->nullableMorphs('subject', 'subject');
+                $table->nullableMorphs('causer', 'causer');
+                $table->json('properties')->nullable();
+                $table->uuid('batch_uuid')->nullable();
+                $table->string('event')->nullable();
+                $table->timestamps();
+                $table->index('log_name');
+            });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
+    public function down(): void
     {
         Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
     }
