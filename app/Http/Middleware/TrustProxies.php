ef877b31574be06835e94e63531a0e504253a048
pacoorozco <paco@pacoorozco.info>
2022-06-13 17:59:40 +0200

    Remove Trusted Proxies configuration
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,24 +1,8 @@
 <?php
-/**
- * SSH Access Manager - SSH keys management solution.
- *
- * Copyright (c) 2017 - 2020 by Paco Orozco <paco@pacoorozco.info>
- *
- *  This file is part of some open source application.
- *
- *  Licensed under GNU General Public License 3.0.
- *  Some rights reserved. See LICENSE, AUTHORS.
- *
- *  @author      Paco Orozco <paco@pacoorozco.info>
- *  @copyright   2017 - 2020 Paco Orozco
- *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
- *
- *  @link        https://github.com/pacoorozco/ssham
- */
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
@@ -26,7 +10,7 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string|null
+     * @var array<int, string>|string|null
      */
     protected $proxies;
 
@@ -35,5 +19,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
