f4ea4c76c8fe05a72c7a76d8c87bdf965845430d
pacoorozco <paco@pacoorozco.info>
2023-03-20 12:57:27 +0100

    Creates a wrap around phpseclib SFTP
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ app/Services/Pusher/Clients/SFTPClient.php
@@ -0,0 +1,183 @@
+<?php
+/*
+ * Copyright (c) 2016-2023 Paco Orozco <paco@pacoorozco.info>
+ *
+ * This file is part of ProBIND v3.
+ *
+ * ProBIND v3 is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * ProBIND v3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ProBIND v3. If not,
+ * see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace App\Services\Pusher\Clients;
+
+use InvalidArgumentException;
+use phpseclib3\Crypt\Common\AsymmetricKey;
+use phpseclib3\Crypt\PublicKeyLoader;
+use phpseclib3\Net\SFTP;
+use RuntimeException;
+
+class SFTPClient
+{
+
+    const FINGERPRINT_MD5 = 'md5';
+    const FINGERPRINT_SHA1 = 'sha1';
+
+    private string $hostname;
+    private int $port = 22;
+    private string $username;
+    private int $timeout = 10;
+    private bool $connected = false;
+    private SFTP $sftp;
+    private AsymmetricKey|string|null $credentials = null;
+
+    public function __construct(string $hostname)
+    {
+        $this->hostname = $hostname;
+    }
+
+    public function as(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function onPort(int $port): self
+    {
+        if ($port < 0) {
+            throw new InvalidArgumentException('Port must be a positive integer.');
+        }
+
+        $this->port = $port;
+        return $this;
+    }
+
+    public function withPassword(string $password): self
+    {
+        $this->credentials = $password;
+        return $this;
+    }
+
+    public function withPrivateKeyFile(string $privateKeyPath): self
+    {
+        try {
+            return $this->withPrivateKey(file_get_contents($privateKeyPath));
+        } catch (\Throwable) {
+            throw new InvalidArgumentException('Invalid private key file specified.');
+        }
+    }
+
+    public function withPrivateKey(string $privateKey): self
+    {
+        try {
+            $this->credentials = PublicKeyLoader::load($privateKey);
+        } catch (\Throwable) {
+            throw new InvalidArgumentException('Invalid private key specified.');
+        }
+        return $this;
+    }
+
+    public function timeout(int $timeout): self
+    {
+        if ($timeout < 0) {
+            throw new InvalidArgumentException('Timeout must be a positive integer.');
+        }
+
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    public function connect(): self
+    {
+        $this->sanityCheck();
+
+        $this->sftp = new SFTP($this->hostname, $this->port, $this->timeout);
+
+        $authenticated = $this->sftp->login($this->username, $this->credentials);
+        if (!$authenticated) {
+            throw new RuntimeException('Error authenticating with the provided credentials.');
+        }
+
+        if ($this->timeout) {
+            $this->sftp->setTimeout($this->timeout);
+        }
+
+        $this->connected = true;
+
+        return $this;
+    }
+
+    private function sanityCheck(): void
+    {
+        if (empty($this->username)) {
+            throw new InvalidArgumentException('Username not specified.');
+        }
+
+        if (empty($this->credentials)) {
+            throw new InvalidArgumentException('No password or private key specified.');
+        }
+    }
+
+    public function disconnect(): void
+    {
+        if (!$this->connected) {
+            throw new RuntimeException('Unable to disconnect. Not yet connected.');
+        }
+
+        $this->sftp->disconnect();
+    }
+
+    public function fingerprint(string $type = self::FINGERPRINT_MD5): string
+    {
+        if (!$this->connected) {
+            throw new RuntimeException('Unable to get fingerprint when not connected.');
+        }
+
+        $hostKey = substr($this->sftp->getServerPublicHostKey(), 8);
+
+        switch ($type) {
+            case 'md5':
+                return strtoupper(md5($hostKey));
+
+            case 'sha1':
+                return strtoupper(sha1($hostKey));
+        }
+
+        throw new InvalidArgumentException('Invalid fingerprint type specified.');
+    }
+
+    public function upload(string $localPath, string $remotePath): bool
+    {
+        if (!$this->connected) {
+            throw new RuntimeException('Unable to upload file when not connected.');
+        }
+
+        if (!file_exists($localPath)) {
+            throw new InvalidArgumentException('The local file does not exist.');
+        }
+
+        return $this->sftp->put($remotePath, $localPath, SFTP::SOURCE_LOCAL_FILE);
+    }
+
+    public function download(string $remotePath, string $localPath): bool
+    {
+        if (!$this->connected) {
+            throw new RuntimeException('Unable to download file when not connected.');
+        }
+
+        return $this->sftp->get($remotePath, $localPath);
+    }
+
+    public function isConnected(): bool
+    {
+        return $this->connected;
+    }
+}
---
+++ tests/Unit/Services/Pusher/Clients/SFTPClientTest.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * ProBIND v3 - Professional DNS management made easy.
+ *
+ * Copyright (c) 2016 by Paco Orozco <paco@pacoorozco.info>
+ *
+ * This file is part of some open source application.
+ *
+ * Licensed under GNU General Public License 3.0.
+ * Some rights reserved. See LICENSE, AUTHORS.
+ *
+ * @author      Paco Orozco <paco@pacoorozco.info>
+ * @copyright   2016 Paco Orozco
+ * @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *
+ * @link        https://github.com/pacoorozco/probind
+ */
+
+namespace Tests\Unit\Services\Pusher\Clients;
+
+use App\Services\Pusher\Clients\SFTPClient;
+use Mockery\MockInterface;
+use phpseclib3\Net\SFTP;
+use Tests\TestCase;
+
+class SFTPClientTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_fail_when_connecting_without_username(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Username not specified.');
+
+        (new SFTPClient('localhost'))
+            ->withPassword('s3cr3t')
+            ->connect();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_connecting_without_credentials(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('No password or private key specified.');
+
+        (new SFTPClient('localhost'))
+            ->as('foo')
+            ->connect();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_setting_port_with_invalid_port(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Port must be a positive integer.');
+
+        (new SFTPClient('localhost'))
+            ->onPort(-1);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_setting_timeout_with_invalid_timeout(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Timeout must be a positive integer.');
+
+        (new SFTPClient('localhost'))
+            ->timeout(-1);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_setting_credentials_with_invalid_private_key_file(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid private key file specified.');
+
+        (new SFTPClient('localhost'))
+            ->withPrivateKeyFile('foo/bar');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_setting_credentials_with_invalid_private_key(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid private key specified.');
+
+        (new SFTPClient('localhost'))
+            ->as('foo')
+            ->withPrivateKey('fooBarBaz');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_disconnecting_from_non_connected_server(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to disconnect. Not yet connected.');
+
+        (new SFTPClient('localhost'))
+            ->disconnect();
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider providesFingerprintTypes
+     */
+    public function it_should_fail_when_getting_fingerprint_from_non_connected_server(
+        string $type
+    ): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to get fingerprint when not connected.');
+
+        (new SFTPClient('localhost'))
+            ->fingerprint($type);
+    }
+
+    public static function providesFingerprintTypes(): \Generator {
+        yield 'MD5 Fingerprint' => [
+            'type' => 'md5'
+        ];
+
+        yield 'SHA1 Fingerprint' => [
+            'type' => 'sha1'
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_uploading_from_non_connected_server(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to upload file when not connected.');
+
+        (new SFTPClient('localhost'))
+            ->upload('source', 'destination');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_fail_when_downloading_from_non_connected_server(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to download file when not connected.');
+
+        (new SFTPClient('localhost'))
+            ->download('source', 'destination');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_false_when_not_connected(): void
+    {
+        $client = (new SFTPClient('localhost'));
+
+        $this->assertFalse($client->isConnected());
+    }
+}
